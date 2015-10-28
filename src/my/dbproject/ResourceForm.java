622f2ca42a7d6aa00f61bf9ae93e8f228ed90d6d
Chintan Panchamia <chintanpanchamia@Chintans-MacBook-Pro.local>
2015-10-27 21:10:43 -0400

    books

--- src/my/dbproject/ResourceForm.java
+++ src/my/dbproject/ResourceForm.java
@@ -168,7 +168,8 @@ public class ResourceForm extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
+        BookSelectionForm.init();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
