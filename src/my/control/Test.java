aea7bd2e4b687ccb101c35ccbb79d06db477f2d5
aagrawa8 <aagrawa8@ncsu.edu>
2015-10-28 20:55:26 -0400

    camera

--- src/my/control/Test.java
+++ src/my/control/Test.java
@@ -5,12 +5,24 @@
  */
 package my.control;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Pooja Asher
  */
 public class Test {
-    public static void main(){
+    public static void main(String arg[]){
+        LibrarySystem l=new LibrarySystem();
+        l.setup();
+        ButtonEvents b= new ButtonEvents();
+        try {
+            b.displayCameras();
+        } catch (SQLException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
 }
